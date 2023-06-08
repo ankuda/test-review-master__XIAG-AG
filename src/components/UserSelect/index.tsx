55c42a7d3b6cf5472eccd84c3965e21ab44a2fe7
ankuda <ankudakirill@gmail.com>
2023-06-09 01:18:15 +0200

    UserSelect component refactoring

--- src/components/UserSelect/index.tsx
+++ src/components/UserSelect/index.tsx
@@ -1,44 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
 import styles from './UserSelect.module.css';
 
-type UserSelectProps = {
-    user?: number,
-    idx: number,
-}
-
-function UserSelect(props: UserSelectProps) {
+const UserSelect: React.FC<UserSelectProps> = ({ idx, user }) => {
     const dispatch = useDispatch();
-    const todos = useSelector((state: {list: { todos: any[] }}) => state.list.todos);
-    React.useEffect(
-        () => {
-            console.log('userSelect');
-            fetch('https://jsonplaceholder.typicode.com/users/').then(
-                (users) => users.json(),
-            ).then(users => setOptions(users))
-        },
-        [],
-    )
-    const [options, setOptions] = React.useState([]);
+    const todos = useSelector((state: AppState) => state.list.todos);
 
-    const { idx } = props;
+    const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        console.log('userSelect');
+        fetch('https://jsonplaceholder.typicode.com/users/').then(
+            (users) => users.json(),
+        ).then(users => setOptions(users))
+    }, []);
+
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         const changedTodos = todos.map((t, index) => {
             const res = { ...t }
-            if (index == idx) {
-                console.log('props.user', props.user);
+            if (index === idx) {
+                console.log('props.user', user);
                 res.user = e.target.value;
             }
             return res;
-        })
+        });
+
         dispatch({type: 'CHANGE_TODO', payload: changedTodos})
-    }
+    };
 
     return (
         <select name="user" className={styles.user} onChange={handleChange}>
-            {options.map((user: any) => <option value={user.id}>{user.name}</option>)}
+            {options.map((user: any) => <option key={user.id} value={user.id}>{user.name}</option>)}
         </select>
     );
-}
+};
 
 export default UserSelect;
